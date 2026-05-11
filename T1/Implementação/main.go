ffa622b8edfca7b28b679fe7a1a13e48909f42c2
Natan De Aragão Müller <natan.muller@edu.pucrs.br>
2026-05-11 19:59:45 -0300

    refactor: remover comentário desnecessário da função renderer

--- T1/Implementação/main.go
+++ T1/Implementação/main.go
@@ -15,7 +15,6 @@ func renderer(renderCh <-chan GameEvent, done <-chan struct{}) {
 			return
 
 		case event := <-renderCh:
-			// limpa terminal
 			fmt.Print("\033[H\033[2J")
 
 			fmt.Println("===================================")
